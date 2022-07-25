366d6afe722ba9f2e47a67b24712d359f3b3201a
selcuklu <yilmax4@gmail.com>
2022-07-25 15:02:36 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import Home from "./components/Home/Home";
 import { Provider } from "react-redux";
 import { takeStore } from "./redux";
-// import FormContainer from "./components/form/Form";
-// import Table from "./components/table/Table";
+
 import React, { useEffect, useState } from "react";
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "./auth/firebaseConfig";
+// import { getDocs, collection } from "firebase/firestore";
+// import { db } from "./auth/firebaseConfig";
 
 function App() {
-  const [personList, setPersonList] = useState([]);
-  const personCollectionRef = collection(db, "person");
-  const getPerson = async () => {
-    const data = await getDocs(personCollectionRef);
-    console.log(data);
-    setPersonList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  };
-  useEffect(() => {
-    getPerson();
-  }, []);
+  // const [personList, setPersonList] = useState([]);
+  // const personCollectionRef = collection(db, "person");
+  // const getPerson = async () => {
+  //   const data = await getDocs(personCollectionRef);
+  //   console.log(data);
+  //   setPersonList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  // };
+  // useEffect(() => {
+  //   getPerson();
+  // }, []);
 
   const store = takeStore();
   return (
