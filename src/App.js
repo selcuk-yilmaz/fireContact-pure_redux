d7d17cb6de43778aa0a481a232bc407cf3fb1892
selcuklu <yilmax4@gmail.com>
2022-07-27 23:23:50 +0300

    firebase studying

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import { Provider } from "react-redux";
 import { takeStore } from "./redux";
 
 import React, { useEffect, useState } from "react";
-// import { getDocs, collection } from "firebase/firestore";
-// import { db } from "./auth/firebaseConfig";
+import { getDocs, collection, addDoc } from "firebase/firestore";
+import { db } from "./auth/firebaseConfig";
+
+import { useSelector } from "react-redux";
 
 function App() {
-  // const [personList, setPersonList] = useState([]);
-  // const personCollectionRef = collection(db, "person");
-  // const getPerson = async () => {
-  //   const data = await getDocs(personCollectionRef);
-  //   console.log(data);
-  //   setPersonList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  // };
-  // useEffect(() => {
-  //   getPerson();
-  // }, []);
+  const todoList = useSelector((state) => state.todoRed.todoList);
+  const [personList, setPersonList] = useState([]);
+  const personCollectionRef = collection(db, "person");
+
+  const createUser = async () => {
+    await addDoc(personCollectionRef, { ...todoList, ...personList });
+  };
+
+  useEffect(() => {
+    const getPerson = async () => {
+      const data = await getDocs(personCollectionRef);
+      console.log(data);
+      setPersonList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPerson();
+  }, []);
 
   const store = takeStore();
   return (
