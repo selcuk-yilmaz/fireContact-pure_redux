58cef3312f5de4ddf3925ba7b7b589cf01d80062
selcuklu <yilmax4@gmail.com>
2022-07-28 16:38:48 +0300

    firebase continue

--- src/redux/reducers/todoReducer.jsx
+++ src/redux/reducers/todoReducer.jsx
@@ -1,4 +1,4 @@
-import { ADD_TODO } from "../types/todoTypes";
+import { ADD_TODO, SET_TODO } from "../types/todoTypes";
 
 const initialState = {
   todoList: [],
@@ -11,12 +11,22 @@ const todoReducer = (state = initialState, { type, payload }) => {
         todoList: [
           ...state.todoList,
           {
-            id: new Date().getTime(),
+            id: payload.id,
             text: payload.name,
             number: payload.number,
             gender: payload.gender,
           },
         ],
+      };
+    case SET_TODO:
+      return {
+        payload.map((data)=>({
+        id: data.id,
+            text:data.name,
+            number: data.number,
+            gender: data.gender,
+        }))
+
       };
 
     default:
